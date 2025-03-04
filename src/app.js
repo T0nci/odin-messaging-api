e90e1e4b89b1f8c1192c9fe34f78f905665aefa6
T0nci <138928016+T0nci@users.noreply.github.com>
2025-03-04 23:05:21 +0100

    feat: Setup the initial structure for the app

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const CustomError = require("./utils/CustomError");
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL],
+  }),
+);
+app.use(cookieParser());
+app.use(express.json());
+
+// if no route matched then it's a 404
+app.use((req, res, next) => next(new CustomError("Not Found", 404)));
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  if (!error.statusCode) {
+    console.log(error);
+    error.statusCode = 500;
+    error.message = "Internal Server Error";
+  }
+
+  res
+    .status(error.statusCode)
+    .json({ error: `${error.statusCode}: ${error.message}` });
+});
+
+module.exports = app;
