8fbc4f9bf010d3cc97a693fc628828eaf94e6a30
T0nci <138928016+T0nci@users.noreply.github.com>
2025-03-10 17:19:22 +0100

    fix: Make refresh tokens last longer

---
+++ prisma/migrations/20250310161901_refresh_token_more_days/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "FriendMessage" ALTER COLUMN "date_sent" SET DEFAULT (now() at time zone 'utc');
+
+-- AlterTable
+ALTER TABLE "Friendship" ALTER COLUMN "date_accepted" SET DEFAULT (now() at time zone 'utc');
+
+-- AlterTable
+ALTER TABLE "GroupMessage" ALTER COLUMN "date_sent" SET DEFAULT (now() at time zone 'utc');
+
+-- AlterTable
+ALTER TABLE "RefreshToken" ALTER COLUMN "expires" SET DEFAULT (now() at time zone 'utc') + interval '7d';
+
+-- AlterTable
+ALTER TABLE "Request" ALTER COLUMN "date_sent" SET DEFAULT (now() at time zone 'utc');
