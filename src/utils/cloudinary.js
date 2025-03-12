e833072cca3e74caff5bc085abcc99bded8f9c8f
T0nci <138928016+T0nci@users.noreply.github.com>
2025-03-12 01:49:41 +0100

    feat: Add and setup multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  secure: true,
+});
+
+const uploadImage = async (image, user_id) => {
+  const response = await cloudinary.uploader.upload(image, {
+    resource_type: "image",
+    asset_folder: "messaging_app",
+
+    // this way it gets overwritten for each user
+    // and I don't need a delete method
+    public_id: user_id,
+    overwrite: true,
+  });
+
+  return response.public_id;
+};
+
+const generateUrl = (public_id) => cloudinary.url(public_id);
+
+module.exports = {
+  uploadImage,
+  generateUrl,
+};
