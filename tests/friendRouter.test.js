d8695adebf8b4efb5abd8fcc87f6590e2ed71a9f
T0nci <138928016+T0nci@users.noreply.github.com>
2025-03-20 13:07:55 +0100

    refactor: Finish refactoring friendRouter tests

--- tests/friendRouter.test.js
+++ tests/friendRouter.test.js
@@ -3,6 +3,7 @@ const request = require("supertest")(app);
 const prisma = require("../src/db/client");
 const { jest: globalJest, describe, it, expect } = require("@jest/globals");
 const { deleteFriends } = require("./data/cleanup");
+const users = require("./data/users");
 
 const cloudinary = require("../src/utils/cloudinary");
 globalJest.mock("../src/utils/cloudinary");
@@ -13,53 +14,39 @@ describe("GET /friends", () => {
     cloudinary.generateUrl.mockReturnValueOnce("some url");
     cloudinary.generateUrl.mockReturnValueOnce("some url");
 
-    const user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
-    const friend1 = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-      include: {
-        profile: {
-          select: {
-            display_name: true,
-          },
-        },
-      },
-    });
-    const friend2 = await prisma.user.findUnique({
-      where: {
-        username: "sam1",
-      },
-      include: {
-        profile: {
-          select: {
-            display_name: true,
-          },
-        },
-      },
-    });
+    const user = users.find((user) => user.username === "penny");
+    const friend1 = users.find((user) => user.username === "al1c3");
+    const friend2 = users.find((user) => user.username === "sam1");
 
-    const friendship1 = await prisma.friendship.create();
-    const friendship2 = await prisma.friendship.create();
+    const friendship1 = await prisma.friendship.create({
+      data: {
+        id: 1,
+      },
+    });
+    const friendship2 = await prisma.friendship.create({
+      data: {
+        id: 2,
+      },
+    });
     await prisma.friend.createMany({
       data: [
         {
+          id: 1,
           friendship_id: friendship1.id,
           user_id: user.id,
         },
         {
+          id: 2,
           friendship_id: friendship1.id,
           user_id: friend1.id,
         },
         {
+          id: 3,
           friendship_id: friendship2.id,
           user_id: user.id,
         },
         {
+          id: 4,
           friendship_id: friendship2.id,
           user_id: friend2.id,
         },
@@ -80,12 +67,12 @@ describe("GET /friends", () => {
     expect(response.body).toEqual(
       [
         {
-          displayName: friend1.profile.display_name,
+          displayName: friend1.profile.create.display_name,
           id: friend1.id,
           picture: "some url",
         },
         {
-          displayName: friend2.profile.display_name,
+          displayName: friend2.profile.create.display_name,
           id: friend2.id,
           picture: "some url",
         },
