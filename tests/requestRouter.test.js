09276fb86dfa876fe663e12c7769ce40787f1b28
T0nci <138928016+T0nci@users.noreply.github.com>
2025-03-20 18:50:57 +0100

    refactor: Refactor requestRouter tests with users

--- tests/requestRouter.test.js
+++ tests/requestRouter.test.js
@@ -3,26 +3,12 @@ const request = require("supertest")(app);
 const prisma = require("../src/db/client");
 const { describe, it, expect } = require("@jest/globals");
 const { deleteFriends, deleteRequests } = require("./data/cleanup");
+const users = require("./data/users");
 
 describe("/requests", () => {
   it("gets received requests", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-      include: {
-        profile: {
-          select: {
-            display_name: true,
-          },
-        },
-      },
-    });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
+    const from_user = users.find((user) => user.username === "al1c3");
+    const to_user = users.find((user) => user.username === "penny");
     await prisma.request.create({
       data: {
         from_id: from_user.id,
@@ -38,31 +24,26 @@ describe("/requests", () => {
       .find((cookie) => cookie.startsWith("access"))
       .split(";")[0];
 
-    await request.post("/requests/" + to_user.id).set("Cookie", [accessToken]);
-
     const response = await request
       .get("/requests")
       .set("Cookie", [accessToken]);
 
     expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
     expect(response.body[0].id).toBe(from_user.id);
-    expect(response.body[0].from).toBe(from_user.profile.display_name);
+    expect(response.body[0].from).toBe(from_user.profile.create.display_name);
     expect(response.body[0].sent).toBeDefined();
 
     await deleteRequests();
   });
 
   it("gets sent requests", async () => {
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-      include: {
-        profile: {
-          select: {
-            display_name: true,
-          },
-        },
+    const from_user = users.find((user) => user.username === "penny");
+    const to_user = users.find((user) => user.username === "al1c3");
+    await prisma.request.create({
+      data: {
+        from_id: from_user.id,
+        to_id: to_user.id,
       },
     });
 
@@ -74,15 +55,13 @@ describe("/requests", () => {
       .find((cookie) => cookie.startsWith("access"))
       .split(";")[0];
 
-    await request.post("/requests/" + to_user.id).set("Cookie", [accessToken]);
-
     const response = await request
       .get("/requests/sent")
       .set("Cookie", [accessToken]);
 
     expect(response.status).toBe(200);
     expect(response.body[0].id).toBe(to_user.id);
-    expect(response.body[0].to).toBe(to_user.profile.display_name);
+    expect(response.body[0].to).toBe(to_user.profile.create.display_name);
     expect(response.body[0].sent).toBeDefined();
 
     await deleteRequests();
@@ -109,11 +88,7 @@ describe("POST /request/:userId", () => {
   });
 
   it("returns error when trying to send request to self", async () => {
-    const user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
+    const user = users.find((user) => user.username === "penny");
 
     const login = await request
       .post("/login")
@@ -133,16 +108,8 @@ describe("POST /request/:userId", () => {
   });
 
   it("returns error when sent request already exists", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-    });
+    const from_user = users.find((user) => user.username === "penny");
+    const to_user = users.find((user) => user.username === "al1c3");
     await prisma.request.create({
       data: {
         from_id: from_user.id,
@@ -170,16 +137,8 @@ describe("POST /request/:userId", () => {
   });
 
   it("returns error when received request already exists", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-    });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
+    const from_user = users.find((user) => user.username === "al1c3");
+    const to_user = users.find((user) => user.username === "penny");
     await prisma.request.create({
       data: {
         from_id: from_user.id,
@@ -207,24 +166,22 @@ describe("POST /request/:userId", () => {
   });
 
   it("returns error when friendship already exists", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
+    const from_user = users.find((user) => user.username === "penny");
+    const to_user = users.find((user) => user.username === "al1c3");
+    const friendship = await prisma.friendship.create({
+      data: {
+        id: 1,
       },
     });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-    });
-    const friendship = await prisma.friendship.create();
     await prisma.friend.createMany({
       data: [
         {
+          id: 1,
           user_id: from_user.id,
           friendship_id: friendship.id,
         },
         {
+          id: 2,
           user_id: to_user.id,
           friendship_id: friendship.id,
         },
@@ -251,16 +208,8 @@ describe("POST /request/:userId", () => {
   });
 
   it("creates request", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-    });
+    const from_user = users.find((user) => user.username === "penny");
+    const to_user = users.find((user) => user.username === "al1c3");
 
     const login = await request
       .post("/login")
@@ -320,7 +269,7 @@ describe("PUT /request/:userId", () => {
       .split(";")[0];
 
     const response = await request
-      .put("/requests/" + 12)
+      .put("/requests/" + 12534)
       .set("Cookie", [accessToken]);
 
     expect(response.status).toBe(400);
@@ -329,16 +278,8 @@ describe("PUT /request/:userId", () => {
   });
 
   it("accepts request and creates a friendship", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-    });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
+    const from_user = users.find((user) => user.username === "al1c3");
+    const to_user = users.find((user) => user.username === "penny");
     await prisma.request.create({
       data: {
         from_id: from_user.id,
@@ -423,7 +364,7 @@ describe("DELETE /request/:userId", () => {
       .split(";")[0];
 
     const response = await request
-      .delete("/requests/" + 12)
+      .delete("/requests/" + 12534)
       .set("Cookie", [accessToken]);
 
     expect(response.status).toBe(400);
@@ -432,16 +373,8 @@ describe("DELETE /request/:userId", () => {
   });
 
   it("deletes received request", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-    });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
+    const from_user = users.find((user) => user.username === "al1c3");
+    const to_user = users.find((user) => user.username === "penny");
     await prisma.request.create({
       data: {
         from_id: from_user.id,
@@ -478,16 +411,8 @@ describe("DELETE /request/:userId", () => {
   });
 
   it("deletes sent request", async () => {
-    const from_user = await prisma.user.findUnique({
-      where: {
-        username: "penny",
-      },
-    });
-    const to_user = await prisma.user.findUnique({
-      where: {
-        username: "al1c3",
-      },
-    });
+    const from_user = users.find((user) => user.username === "penny");
+    const to_user = users.find((user) => user.username === "al1c3");
     await prisma.request.create({
       data: {
         from_id: from_user.id,
