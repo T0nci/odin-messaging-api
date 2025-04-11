021878ca1e4f4de526c6f0efc1c70a51a014c8a1
T0nci <138928016+T0nci@users.noreply.github.com>
2025-04-12 00:22:33 +0200

    fix: Extract duplicate and brittle test code

--- tests/messageRouter.test.js
+++ tests/messageRouter.test.js
@@ -1,7 +1,14 @@
 const app = require("../src/app");
 const request = require("supertest")(app);
 const prisma = require("../src/db/client");
-const { jest: globalJest, describe, it, expect } = require("@jest/globals");
+const {
+  jest: globalJest,
+  describe,
+  it,
+  expect,
+  beforeAll,
+  afterAll,
+} = require("@jest/globals");
 const { deleteFriends } = require("./data/cleanup");
 const users = require("./data/users");
 
@@ -12,6 +19,33 @@ cloudinary.uploadImage = globalJest.fn();
 cloudinary.generateUrl = globalJest.fn();
 
 describe("POST /messages/:userId", () => {
+  const sender = users.find((user) => user.username === "penny");
+  const receiver = users.find((user) => user.username === "al1c3");
+
+  beforeAll(async () => {
+    const friendship = await prisma.friendship.create({
+      data: {
+        id: 1,
+      },
+    });
+    await prisma.friend.createMany({
+      data: [
+        {
+          friendship_id: friendship.id,
+          user_id: sender.id,
+        },
+        {
+          friendship_id: friendship.id,
+          user_id: receiver.id,
+        },
+      ],
+    });
+  });
+
+  afterAll(async () => {
+    await deleteFriends();
+  });
+
   it("returns error if parameter isn't a number or is invalid", async () => {
     const login = await request
       .post("/login")
@@ -56,7 +90,7 @@ describe("POST /messages/:userId", () => {
       .split(";")[0];
 
     const response = await request
-      .post("/messages/" + users.find((user) => user.username === "al1c3").id)
+      .post("/messages/" + users.find((user) => user.username === "sam1").id)
       .set("Cookie", [accessToken]);
 
     expect(response.status).toBe(400);
@@ -64,26 +98,6 @@ describe("POST /messages/:userId", () => {
   });
 
   it("returns error if invalid type", async () => {
-    const sender = users.find((user) => user.username === "penny");
-    const receiver = users.find((user) => user.username === "al1c3");
-    const friendship = await prisma.friendship.create({
-      data: {
-        id: 1,
-      },
-    });
-    await prisma.friend.createMany({
-      data: [
-        {
-          friendship_id: friendship.id,
-          user_id: sender.id,
-        },
-        {
-          friendship_id: friendship.id,
-          user_id: receiver.id,
-        },
-      ],
-    });
-
     const login = await request
       .post("/login")
       .send({ username: "penny", password: "pen@5Apple" });
@@ -93,37 +107,15 @@ describe("POST /messages/:userId", () => {
       .split(";")[0];
 
     const response = await request
-      .post("/messages/" + users.find((user) => user.username === "al1c3").id)
+      .post("/messages/" + receiver.id)
       .set("Cookie", [accessToken])
       .field("type", "blah");
 
     expect(response.status).toBe(400);
     expect(response.body.errors[0].msg).toBe("Unknown message type.");
-
-    await deleteFriends();
   });
 
   it("returns error if no or invalid content", async () => {
-    const sender = users.find((user) => user.username === "penny");
-    const receiver = users.find((user) => user.username === "al1c3");
-    const friendship = await prisma.friendship.create({
-      data: {
-        id: 1,
-      },
-    });
-    await prisma.friend.createMany({
-      data: [
-        {
-          friendship_id: friendship.id,
-          user_id: sender.id,
-        },
-        {
-          friendship_id: friendship.id,
-          user_id: receiver.id,
-        },
-      ],
-    });
-
     const login = await request
       .post("/login")
       .send({ username: "penny", password: "pen@5Apple" });
@@ -133,7 +125,7 @@ describe("POST /messages/:userId", () => {
       .split(";")[0];
 
     const response = await request
-      .post("/messages/" + users.find((user) => user.username === "al1c3").id)
+      .post("/messages/" + receiver.id)
       .set("Cookie", [accessToken])
       .field("type", "text");
 
@@ -141,31 +133,9 @@ describe("POST /messages/:userId", () => {
     expect(response.body.errors[0].msg).toBe(
       "Content must be at least 1 character long.",
     );
-
-    await deleteFriends();
   });
 
   it("returns error if no or invalid image", async () => {
-    const sender = users.find((user) => user.username === "penny");
-    const receiver = users.find((user) => user.username === "al1c3");
-    const friendship = await prisma.friendship.create({
-      data: {
-        id: 1,
-      },
-    });
-    await prisma.friend.createMany({
-      data: [
-        {
-          friendship_id: friendship.id,
-          user_id: sender.id,
-        },
-        {
-          friendship_id: friendship.id,
-          user_id: receiver.id,
-        },
-      ],
-    });
-
     const login = await request
       .post("/login")
       .send({ username: "penny", password: "pen@5Apple" });
@@ -175,13 +145,11 @@ describe("POST /messages/:userId", () => {
       .split(";")[0];
 
     const response = await request
-      .post("/messages/" + users.find((user) => user.username === "al1c3").id)
+      .post("/messages/" + receiver.id)
       .set("Cookie", [accessToken])
       .field("type", "image");
 
     expect(response.status).toBe(400);
     expect(response.body.errors[0].msg).toBe("Image must be provided.");
-
-    await deleteFriends();
   });
 });
